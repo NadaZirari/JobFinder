bce80fb38bbc7001dc97ef33361a21d176140a31
Nada Zirari <zirarinada20@gmail.com>
2026-02-09 14:54:43 +0100

    add rouutes for job search

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,26 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+import { Routes } from '@angular/router';
 
-
-
-
+export const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'jobs',
+    pathMatch: 'full'
+  },
+  {
+    path: 'jobs',
+    loadChildren: () =>
+      import('./jobs/jobs.routes').then(m => m.JOBS_ROUTES)
+  },
+  {
+    path: 'login',
+    loadComponent: () =>
+      import('./auth/login/login.component').then(c => c.LoginComponent)
+  },
+  {
+    path: 'register',
+    loadComponent: () =>
+      import('./auth/register/register.component').then(c => c.RegisterComponent)
+  }
+];
